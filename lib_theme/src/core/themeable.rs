7168e30082c41eec1f61e36133642d8dfc10ab4d
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2023-11-08 12:59:01 +0000

    Add theme post apply action.

--- lib_theme/src/core/themeable.rs
+++ lib_theme/src/core/themeable.rs
@@ -5,8 +5,10 @@ use super::theme::Theme;
 pub trait Themeable {
     fn path(&self) -> PathBuf;
     fn content(&self, theme: Theme) -> String;
+    fn run_post_apply_action(&self) {}
     fn apply(&self, theme: Theme) {
         let mut file = std::fs::File::create(self.path()).unwrap();
         let _ = file.write_all(self.content(theme).as_bytes());
+        self.run_post_apply_action();
     }
 }
--- theme/src/programs/waybar.rs
+++ theme/src/programs/waybar.rs
@@ -14,4 +14,11 @@ impl Themeable for Waybar {
     fn content(&self, theme: lib_theme::core::theme::Theme) -> String {
         theme.css_definitions()
     }
+    
+    fn run_post_apply_action(&self) {
+        let _ = std::process::Command::new("pkill")
+            .arg("-SIGUSR2")
+            .arg("waybar")
+            .spawn();
+    }
 }
